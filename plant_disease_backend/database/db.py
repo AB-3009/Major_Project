41e247422c1d83677618d28f2cccc8e281836f3a
Ayush200230 <humbleayush30@gmail.com>
2024-12-13 23:27:15 +0530

    test commit

--- plant_disease_backend/database/db.py
+++ plant_disease_backend/database/db.py
@@ -1,6 +1,10 @@
 from pymongo import MongoClient
 
 def get_db():
-    mongo_client = MongoClient("mongodb+srv://ayush:ayush@minortry.1bb9ycf.mongodb.net/?retryWrites=true&w=majority&appName=minortry")
+    mongo_client = MongoClient("mongodb+srv://ayush:ayush@minortry.1bb9ycf.mongodb.net/?retryWrites=true&w=majority&appName=minortry",
+        connectTimeoutMS=30000,
+        socketTimeoutMS=None,
+        connect=False,
+        maxPoolSize=1)
     db = mongo_client['plant_disease_db']
     return db
